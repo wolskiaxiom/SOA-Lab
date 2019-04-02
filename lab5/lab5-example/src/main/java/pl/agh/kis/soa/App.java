cfa75f1a38b7d9967126251d9bbeca4278a9a5e9
wolskiaxiom <wolski.luka@gmail.com>
2019-04-02 15:15:00 +0200

    complete example exercise

--- lab5/lab5-example/src/main/java/pl/agh/kis/soa/App.java
+++ lab5/lab5-example/src/main/java/pl/agh/kis/soa/App.java
@@ -1,5 +1,12 @@
 package pl.agh.kis.soa;
 
+import pl.agh.kis.soa.model.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello JPA!" );
+        EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("JPA-SOA-LAB");
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        try {
+            Student s1 = new Student("Anna", "Kowalska", new Date());
+            Student s2 = new Student("Jan", "Kowalski", new Date());
+            Student s3 = new Student("Piotr", "Nowak", new Date());
+
+            em.getTransaction().begin();
+            em.persist(s1);
+            em.persist(s2);
+            em.persist(s3);
+
+            em.getTransaction().commit();
+
+            System.out.println("Check your db");
+        }catch (Exception e){
+            System.err.println("Error" + e);
+        }
+
+
     }
 }
---
+++ lab5/lab5-example/src/main/java/pl/agh/kis/soa/model/Student.java
@@ -0,0 +1,65 @@
+package pl.agh.kis.soa.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Date joinDate;
+
+
+    public Student() {
+        super();
+    }
+
+
+    public Student(String firstName, String lastName, Date joinDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.joinDate = joinDate;
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Column(name = "firstName",nullable = false)
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = true)
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+}
