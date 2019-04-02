f699a0d8cb08f57dc3f050a8db1b8d9b7657b3e6
wolskiaxiom <wolski.luka@gmail.com>
2019-04-02 14:11:58 +0200

    lab5 init

---
+++ lab5/lab5-example/src/main/java/pl/agh/kis/soa/App.java
@@ -0,0 +1,13 @@
+package pl.agh.kis.soa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
