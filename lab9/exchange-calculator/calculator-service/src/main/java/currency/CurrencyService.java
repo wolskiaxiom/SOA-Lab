98123236f9f3aaf7aee3ed6c5aa59fdc50fabf11
Łukasz <wolskiaxiom@interia.pl>
2019-05-21 18:18:24 +0200

    add currency service

---
+++ lab9/exchange-calculator/calculator-service/src/main/java/currency/CurrencyService.java
@@ -0,0 +1,36 @@
+package currency;
+
+import model.CurrencyEntity;
+import model.Rate;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
+@Stateless
+public class CurrencyService {
+    private Client client;
+
+    public double getExchangeRate(String symbol){
+        client = ClientBuilder.newClient();
+        URI uri = UriBuilder.fromUri("http://api.nbp.pl/api/exchangerates/rates/a/"+ symbol+"/").build();
+        Response response = client.target(uri).request().accept(MediaType.APPLICATION_JSON_TYPE).get();
+        if(response.getStatus()!=200){
+            throw new RuntimeException("Error");
+        }
+
+        CurrencyEntity currencyEntity = response.readEntity(CurrencyEntity.class);
+        Rate rate = currencyEntity.getRates().get(0);
+
+        return Double.parseDouble(rate.getMid());
+    }
+
+    public double getAmountOfPLN(int amount, String symbol){
+        return amount * getExchangeRate(symbol);
+    }
+
+}
---
+++ lab9/exchange-calculator/calculator-service/src/main/java/service/CalculatorService.java
@@ -0,0 +1,33 @@
+package service;
+
+import currency.CurrencyService;
+
+import javax.inject.Inject;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class CalculatorService {
+
+    @Inject
+    CurrencyService currencyService ;
+
+
+    @WebMethod
+    public double getExchangeRate(String symbol){
+//        return currencyService.getExchangeRate(symbol);
+        currencyService.getExchangeRate("chf");
+        System.out.println();
+        return 0d;
+    }
+
+    @WebMethod
+    public double getAmountOfPLN(int amount, String symbol){
+        return currencyService.getAmountOfPLN(amount, symbol);
+    }
+
+    public static void main(String[] args) {
+        CalculatorService calculatorService = new CalculatorService();
+        System.out.println(calculatorService.getExchangeRate("chf"));
+    }
+}
