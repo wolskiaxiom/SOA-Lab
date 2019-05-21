f2b120aa88dc6d8778bb5a5d16194fe435d6646f
Łukasz <wolskiaxiom@interia.pl>
2019-05-21 18:17:52 +0200

    add currency entity for rest requests

---
+++ lab9/exchange-calculator/currency-model/src/main/java/model/Rate.java
@@ -0,0 +1,70 @@
+package model;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "no",
+        "effectiveDate",
+        "mid"
+})
+public class Rate {
+
+    @JsonProperty("no")
+    private String no;
+    @JsonProperty("effectiveDate")
+    private String effectiveDate;
+    @JsonProperty("mid")
+    private String mid;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("no")
+    public String getNo() {
+        return no;
+    }
+
+    @JsonProperty("no")
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    @JsonProperty("effectiveDate")
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    @JsonProperty("effectiveDate")
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    @JsonProperty("mid")
+    public String getMid() {
+        return mid;
+    }
+
+    @JsonProperty("mid")
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
