9c92b6949d611cb5585eef447b2c32760ba99c19
Łukasz <wolskiaxiom@interia.pl>
2019-06-11 00:14:30 +0200

    add entities for user

---
+++ parking-meter-project/parking-meter-core/dashboard/src/main/java/pl/agh/soa/dashboard/database/UserManager.java
@@ -0,0 +1,35 @@
+package pl.agh.soa.dashboard.database;
+
+import pl.agh.soa.dashboard.entity.User;
+
+import javax.persistence.EntityManager;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class UserManager {
+    private static EntityManager entityManager = DatabaseService.getInstance().getEntityManager();
+
+    public static User getUser(String login){
+        return entityManager.find(User.class, login);
+    }
+
+    public static void updateUser(User user){
+        entityManager.getTransaction().begin();
+        User updatedUser = entityManager.find(User.class, user.getLogin());
+        updatedUser.setPassword(checkMD5(user.getPassword()));
+        entityManager.getTransaction().commit();
+    }
+
+    public static String checkMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(input.getBytes());
+            byte[] enc = md.digest();
+            String md5Sum = new sun.misc.BASE64Encoder().encode(enc);
+            return md5Sum;
+        } catch (NoSuchAlgorithmException nsae) {
+            System.out.println(nsae.getMessage());
+            return null;
+        }
+    }
+}
