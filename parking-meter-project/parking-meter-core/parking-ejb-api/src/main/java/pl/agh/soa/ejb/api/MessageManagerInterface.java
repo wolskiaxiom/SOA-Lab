bf58bc21abf49648f627b8c9c492aea5a0e15c79
Łukasz <wolskiaxiom@interia.pl>
2019-06-14 14:33:27 +0200

    cleanup

--- parking-meter-project/parking-meter-core/parking-ejb-api/src/main/java/pl/agh/soa/ejb/api/MessageManagerInterface.java
+++ parking-meter-project/parking-meter-core/parking-ejb-api/src/main/java/pl/agh/soa/ejb/api/MessageManagerInterface.java
@@ -5,8 +5,6 @@ import pl.agh.soa.ejb.exceptions.NoSuchParkingSpotException;
 import pl.agh.soa.model.SensorSignal;
 import pl.agh.soa.model.Ticket;
 
-import javax.annotation.security.DeclareRoles;
-import javax.annotation.security.RolesAllowed;
 import javax.ejb.Remote;
 
 @Remote
